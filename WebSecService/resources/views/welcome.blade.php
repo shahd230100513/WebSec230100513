b5c3bdfd12713ab01d47b5aeeded655a4188763a
shahd230100513 <shahd.a.nasr2004@gmail.com>
2025-02-23 08:01:11 +0200

    Assignment 1

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Welcome')
 @section('content')
     <div class="card m-4">
-        <div class="card-header">Welcome To Home Page</div>
+        <div class="card-header">Welcome To The Home Page</div>
     </div>
     </div>
 @endsection
